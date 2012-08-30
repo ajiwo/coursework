402be610dc0fa1f66b90183c504ca422d1d92779
Amin Riza <aminriza@gmail.com>
2012-08-30 18:25:27 +0700

    chapter 1

---
+++ ch1.rb
@@ -0,0 +1,10 @@
+# hours in a year
+puts 24 * 365
+# minutes in a decade
+puts 60 * 24 * 365 * 10
+# my age in seconds
+puts 60 * 60 * 24 * 365 * 30
+# chocolates?
+puts 60 * 60 * 24 * 365 * (30 - 5) * 2
+# seconds to age (years)
+puts 1031000000 / (60 * 60 * 24 * 365)
