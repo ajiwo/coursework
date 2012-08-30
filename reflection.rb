5261ff1fbcd2fa77e08fc6e126c8c3735a1edb8d
Amin Riza <aminriza@gmail.com>
2012-08-30 20:14:08 +0700

    reflection

---
+++ reflection.rb
@@ -0,0 +1,25 @@
+
+# 1
+# puts adds extra newline characters
+# print does not
+
+# 2
+# underscore to separate thousands
+1_000_000
+3_600
+
+# 3
+# to_s: returns string of the calling object
+# to_i: rerurns integer of the calling object
+# puts: put the following string plus newline on the standar output 
+# gets: get the following string with the newline from the standar input
+# chomp: remove the trailing newline from gets call
+
+# 4
+# interpolation
+
+# extra
+# everything in ruby is object
+# because both numbers and string have method +
+# actually we use method call instead of operator expression
+
