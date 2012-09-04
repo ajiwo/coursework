b9801461fce0327d01bff2ad4d7559fedb894925
Amin Riza <aminriza@gmail.com>
2012-09-04 19:55:57 +0700

    my billion turn

---
+++ ch9-1.rb
@@ -0,0 +1,8 @@
+# one billion seconds..
+
+born = Time.mktime(1981, 9, 30, 23, 45)
+billion = 1_000_000_000
+turn = born + billion
+# it say that my age a billion seconds at
+# Sun Jun 09 01:31:40 2013
+puts turn
